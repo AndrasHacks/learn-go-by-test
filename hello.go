f35dc8df23738b63789c28f94d9a9705dd6c2269
AndrasHacks <mr.acs.andras@gmail.com>
2020-06-01 10:57:21 +0200

    Added Spanish and French

--- hello.go
+++ hello.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+	const spanish = "Spanish"
+	const french = "French"
 	const englishHelloPrefix = "Hello, "
+	const spanishHelloPrefix = "Hola, "
+	const frenchHelloPrefix = "Bonjur, "
 	if len(name) == 0 {
-		return englishHelloPrefix + "World!"
+		name = "World"
+	}
+	if language == french {
+		return frenchHelloPrefix + name + "!"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name + "!"
 	}
 	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Printf(Hello(""))
+	fmt.Printf(Hello("", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,24 +4,39 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	// We might get rid of the *testing.T argument by applying partial application
+	assertEquals := func(got string, want string, t *testing.T) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Got %q want %q", got, want)
+		}
+	}
+
 	t.Run("Given empty string, when Hello(), then prints Hello, World!", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
 
 		assertEquals(got, want, t)
 	})
 
 	t.Run("Given a name, when Hello(), then greets name.", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris!"
 
 		assertEquals(got, want, t)
 	})
 
-}
+	t.Run("Given Spanish language, when Hello(), greets in spanish!", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
 
-func assertEquals(got string, want string, t *testing.T) {
-	if got != want {
-		t.Errorf("Got %q want %q", got, want)
-	}
+		assertEquals(got, want, t)
+	})
+
+	t.Run("Given French language, when Hello(), greets in French.", func(t *testing.T) {
+		got := Hello("Claude", "French")
+		want := "Bonjur, Claude!"
+		assertEquals(got, want, t)
+	})
+
 }
