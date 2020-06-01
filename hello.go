f8b569c067e07a12477b1122f63c63121e8fbddc
AndrasHacks <mr.acs.andras@gmail.com>
2020-06-01 11:06:14 +0200

    Swithc case, return value

--- hello.go
+++ hello.go
@@ -2,23 +2,32 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjur, "
+
 func Hello(name string, language string) string {
-	const spanish = "Spanish"
-	const french = "French"
-	const englishHelloPrefix = "Hello, "
-	const spanishHelloPrefix = "Hola, "
-	const frenchHelloPrefix = "Bonjur, "
 	if len(name) == 0 {
 		name = "World"
 	}
-	if language == french {
-		return frenchHelloPrefix + name + "!"
-	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name + "!"
+	return greetingPrefix(language) + name + "!"
+}
+
+// Introduction of a named return value!
+// Private functions starts with lowercase letters.
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name + "!"
+	return prefix
 }
 
 func main() {
