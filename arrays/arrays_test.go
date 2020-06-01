13b13cb069efcdb49ea0c161ba5442471cddd8d1
AndrasHacks <mr.acs.andras@gmail.com>
2020-06-01 19:43:45 +0200

    From arrays to slices

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSum(t *testing.T) {
 
-	assertEquals := func(got int, want int, array [5]int, t *testing.T) {
+	assertEquals := func(got int, want int, array []int, t *testing.T) {
 		t.Helper()
 		if got != want {
 			t.Errorf("Got %d, but wanted %d, from %v", got, want, array)
@@ -14,10 +14,19 @@ func TestSum(t *testing.T) {
 	}
 
 	t.Run("Given an array of number, when Sum(), then gets the sum of all numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
+
+		assertEquals(got, want, numbers, t)
+	})
+
+	t.Run("Given varying length input, When Sum(), Then returns the sum of the numebrs.", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
 
 		assertEquals(got, want, numbers, t)
 	})
