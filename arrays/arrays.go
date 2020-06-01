567c9944f2813c264ec0c0dfbc224ddae9ace841
AndrasHacks <mr.acs.andras@gmail.com>
2020-06-01 20:05:33 +0200

    variadic functions and for

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -7,3 +7,27 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	//lengthOfNumbers := len(numbersToSum)
+	// Create a slice, with the starting capacity of
+	// sums = make([]int, lengthOfNumbers)
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		var tails []int
+		for idx, number := range numbers {
+			if idx > 0 {
+				tails = append(tails, number)
+			}
+		}
+		sums = append(sums, Sum(tails))
+	}
+	return sums
+}
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -29,5 +30,23 @@ func TestSum(t *testing.T) {
 		want := 6
 
 		assertEquals(got, want, numbers, t)
+	})
+
+	t.Run("Given two slices with numbers entered, When SumAll(), Then a slice returned with the sums in it", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{3, 4})
+		want := []int{3, 7}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v, but wanted %v", got, want)
+		}
+	})
+
+	t.Run("Given two slices, When SumAllTails(), Then a slice with the sum of tails returned.", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6})
+		want := []int{5, 11}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v, but wanted %v", got, want)
+		}
 	})
 }
